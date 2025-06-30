df1c976882e465c0f1cf3ddfb3578e8d4fa5148a
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-06-30 05:33:46 +0530

    feat(binds): add toggle binds for low power mode
    Adds hyper + i for wifi, hyper + b for bluetooth, and hyper + e for
    low power mode toggles.

--- myCfg/binds.lua
+++ myCfg/binds.lua
@@ -15,21 +15,49 @@ Bind(TLKeys.hyper, "l", nil, function()
   app.launchOrFocusByBundleID(bundleID.launchpad)
 end)
 
+-- Wifi
 local wifi = require "hs.wifi"
 Bind(TLKeys.hyper, "i", nil, function()
   local status = wifi.interfaceDetails().power
   wifi.setPower(not status)
 end)
 
+-- Bluetooth
 local blueutilOutput, _, _, _ = hs.execute("which blueutil", true)
 local blueutilBin = string.gsub(blueutilOutput, "%s+", "")
 local function blueutil(args)
   local command = string.format("%s %s", blueutilBin, args)
   print(string.format("blueutil: %s", command))
-  local out, _, _, _ = hs.execute(command, false)
-  return out
+  os.execute(command)
+end
+local function getStatusBluetooth()
+  local status_cmd = string.format("%s -p", blueutilBin)
+  local status, _, _, _ = hs.execute(status_cmd, false)
+  return tonumber(status)
 end
 Bind(TLKeys.hyper, "b", nil, function()
-  local status = tonumber(blueutil "-p")
-  blueutil(string.format("-p %s", status == 0 and 1 or 0))
+  local new_status = getStatusBluetooth() == 0 and 1 or 0
+  local set_cmd = string.format("-p %s", new_status)
+  blueutil(set_cmd)
+end)
+
+-- Low Power Mode
+local rgOutput, _, _, _ = hs.execute("which rg", true)
+local rgBin = string.gsub(rgOutput, "%s+", "")
+local awkOutput, _, _, _ = hs.execute("which awk", true)
+local awkBin = string.gsub(awkOutput, "%s+", "")
+local function pmset(args)
+  local command = string.format("/usr/bin/sudo /usr/bin/pmset %s", args)
+  print(string.format("pmset: %s", command))
+  os.execute(command)
+end
+local function getStatusLowPowerMode()
+  local status_cmd = string.format("/usr/bin/pmset -g | %s lowpowermode | %s '{print $2}'", rgBin, awkBin)
+  local status, _, _, _ = hs.execute(status_cmd, false)
+  return tonumber(status)
+end
+Bind(TLKeys.hyper, "e", nil, function()
+  local new_status = getStatusLowPowerMode() == 0 and 1 or 0
+  local set_cmd = string.format("-a lowpowermode %s", new_status)
+  pmset(set_cmd)
 end)
