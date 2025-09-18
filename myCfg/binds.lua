1915a61a786cd0b33707c69e7d6e2e4bd2a2a3cb
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-09-18 20:02:34 +0530

    feat(binds): launch wezterm via shell command

--- myCfg/binds.lua
+++ myCfg/binds.lua
@@ -5,14 +5,22 @@ TLKeys.hyper = { "alt", "ctrl", "shift", "cmd" }
 
 local app = require "hs.application"
 local bundleID = {
-  wezterm = "com.github.wez.wezterm",
   launchpad = "com.apple.launchpad.launcher",
 }
-Bind(TLKeys.hyper, "return", nil, function()
-  app.launchOrFocusByBundleID(bundleID.wezterm)
-end)
 Bind(TLKeys.hyper, "l", nil, function()
   app.launchOrFocusByBundleID(bundleID.launchpad)
+end)
+
+-- Wezterm
+local weztermOutput, _, _, _ = hs.execute("which wezterm", true)
+local weztermBin = string.gsub(weztermOutput, "%s+", "")
+local function wezterm(args)
+  local command = string.format("%s %s", weztermBin, args)
+  print(string.format("wezterm: %s", command))
+  os.execute(command)
+end
+Bind(TLKeys.hyper, "return", nil, function()
+  wezterm "start"
 end)
 
 -- Wifi
