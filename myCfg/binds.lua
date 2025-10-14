cfa225a68cd05b81fb5b250c80078eb7b9253329
Divit Mittal <64.69.76.69.74.m@gmail.com>
2025-10-14 19:20:58 +0530

    feat(binds): add hyper key bindings for firefox & fix wezterm binding

--- myCfg/binds.lua
+++ myCfg/binds.lua
@@ -3,36 +3,34 @@ Bind = require("hs.hotkey").bind
 TLKeys = {}
 TLKeys.hyper = { "alt", "ctrl", "shift", "cmd" }
 
-local app = require "hs.application"
+-- App Launcher (add Hammerspoon to App Management in System Settings > Security & Privacy)
 local bundleID = {
   launchpad = "com.apple.launchpad.launcher",
+  firefox = "org.mozilla.firefox",
+  wezterm = "com.github.wez.wezterm",
 }
+local app = require "hs.application"
 Bind(TLKeys.hyper, "l", nil, function()
   app.launchOrFocusByBundleID(bundleID.launchpad)
 end)
-
--- Wezterm
-local weztermOutput, _, _, _ = hs.execute("which wezterm", true)
-local weztermBin = string.gsub(weztermOutput, "%s+", "")
-local function wezterm(args)
-  local command = string.format("%s %s", weztermBin, args)
-  print(string.format("wezterm: %s", command))
-  os.execute(command)
-end
+Bind(TLKeys.hyper, "f", nil, function()
+  app.launchOrFocusByBundleID(bundleID.firefox)
+end)
 Bind(TLKeys.hyper, "return", nil, function()
-  wezterm "start"
+  app.launchOrFocusByBundleID(bundleID.wezterm)
 end)
 
--- Wifi
-local wifi = require "hs.wifi"
+-- Wifi Toggle
 Bind(TLKeys.hyper, "i", nil, function()
+  local wifi = require "hs.wifi"
   local status = wifi.interfaceDetails().power
   wifi.setPower(not status)
 end)
 
--- Bluetooth
+-- Bluetooth Toggle
 local blueutilOutput, _, _, _ = hs.execute("which blueutil", true)
 local blueutilBin = string.gsub(blueutilOutput, "%s+", "")
+
 local function blueutil(args)
   local command = string.format("%s %s", blueutilBin, args)
   print(string.format("blueutil: %s", command))
@@ -43,17 +41,19 @@ local function getStatusBluetooth()
   local status, _, _, _ = hs.execute(status_cmd, false)
   return tonumber(status)
 end
+
 Bind(TLKeys.hyper, "b", nil, function()
   local new_status = getStatusBluetooth() == 0 and 1 or 0
   local set_cmd = string.format("-p %s", new_status)
   blueutil(set_cmd)
 end)
 
--- Low Power Mode
+-- Low Power Mode Toggle
 local rgOutput, _, _, _ = hs.execute("which rg", true)
 local rgBin = string.gsub(rgOutput, "%s+", "")
 local awkOutput, _, _, _ = hs.execute("which awk", true)
 local awkBin = string.gsub(awkOutput, "%s+", "")
+
 local function pmset(args)
   local command = string.format("/usr/bin/sudo /usr/bin/pmset %s", args)
   print(string.format("pmset: %s", command))
@@ -64,6 +64,7 @@ local function getStatusLowPowerMode()
   local status, _, _, _ = hs.execute(status_cmd, false)
   return tonumber(status)
 end
+
 Bind(TLKeys.hyper, "e", nil, function()
   local new_status = getStatusLowPowerMode() == 0 and 1 or 0
   local set_cmd = string.format("-a lowpowermode %s", new_status)
